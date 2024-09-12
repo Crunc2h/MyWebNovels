c9618187481a38cff5780fc17542c41b18cce218
Crunc2h <omrsythngl@hotmail.com>
2024-09-12 20:53:57 +0300

    scrapping the project and starting again with new repo, it got unnecessarily complex, I am going to dumb it down and make it more sync, also scrapy migration is kind of disruptive so

--- spiders/native/sc_spiders/spiders/novel_links_collector_spider.py
+++ spiders/native/sc_spiders/spiders/novel_links_collector_spider.py
@@ -11,7 +11,9 @@ BASE_LINK_TEST = "https://www.webnovelpub.pro"
 class WebnovelpubNovelLinksCollector(scrapy.Spider):
     name = "webnovelpub__links_collector"
     custom_settings = {"USER_AGENT": UA.chrome}
-    file_str = "sc_spiders/extracted_data/links.txt"
+    file_str = "sc_spiders/extracted_data/webnovelpub/links.txt"
+    current_page = 1
+    max_page = 0
 
     def start_requests(self):
         if os.path.exists(self.file_str):
@@ -25,6 +27,14 @@ class WebnovelpubNovelLinksCollector(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
+        if self.max_page == 0:
+            self.max_page = int(
+                response.css(".PagedList-skipToLast > a:nth-child(1)::attr(href)")
+                .get()
+                .split("page=")[1]
+            )
+        print(f"------------------------------------->{self.max_page}")
+        print(f"------------------------------------->{self.current_page}")
         novels_on_page = response.css("li.novel-item a::attr(href)").getall()
         with open(self.file_str, "a") as file:
             file.write("\n".join(novels_on_page))
@@ -33,7 +43,11 @@ class WebnovelpubNovelLinksCollector(scrapy.Spider):
             ".PagedList-skipToNext > a:nth-child(1)::attr(href)"
         ).get()
         if next_page is not None:
+            self.current_page += 1
             yield response.follow(next_page, self.parse)
+        else:
+            if self.current_page != self.max_page:
+                print("WWWWWWWWWWWWWWWWTTTTTTTTTTTTTTTTTTTTTFFFFFFFFFFFFFFFFFFFFF")
 
     @classmethod
     def from_crawler(cls, crawler, *args, **kwargs):
